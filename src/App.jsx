ee7230da07dff4891cea561d5d83d5dfc5ba58be
caesdigi <caesdigi@hku.hk>
2025-06-06 01:00:15 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import CalendarView from './components/CalendarView';
 import CalendarView_Embed from './components/CalendarView_Embed';
+import Equipment_Wrapper from './components/equipment/Equipment_Wrapper';
+import Equipment_Wrapper_Embed from './components/equipment/Equipment_Wrapper_Embed';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/classroom" element={<CalendarView />} />
         <Route path="/classroom_embed" element={<CalendarView_Embed />} />
+        <Route path="/equipment" element={<Equipment_Wrapper />} />
+        <Route path="/equipment_embed" element={<Equipment_Wrapper_Embed />} />
         <Route path="*" element={<LandingPage />} />
       </Routes>
     </Router>
